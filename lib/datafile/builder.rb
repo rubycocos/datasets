812161f1689cd0643309687229cade49f73efa0d
Gerald Bauer <gerald.bauer@gmail.com>
2015-08-27 10:34:16 +0200

    incl. world

--- lib/datafile/builder.rb
+++ lib/datafile/builder.rb
@@ -18,6 +18,20 @@ class Builder     ## "simple" builder (one file, one datafile)
 
   include LogUtils::Logging
 
+  ## add models shortcuts/constants if exist
+  ##   todo/check: is there a better way ?
+  ##    e.g. just include in inline section/block - for example?
+  ##    check if already included? and check on load to include?
+  ##     models might not yet be required
+
+  if defined?( WorldDb ) && defined?( WorldDb::Models )
+    include WorldDb::Models
+  else
+    puts "*** sorry; can't include WorldDb::Models; not yet defined (required)"
+  end
+  ## todo/check: also include FootballDb::Models and BeerDb::Models ???
+
+
   def initialize
     @datafile = Datafile.new
   end
